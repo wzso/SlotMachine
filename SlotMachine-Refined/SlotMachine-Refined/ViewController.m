cd2b44cd1279bd47e95185e8de998db6f42d989c
Vincent <wenzhishen@icloud.com>
2017-06-23 16:19:58 +0800

    refine impl

---
+++ SlotMachine-Refined/SlotMachine-Refined/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SlotMachine-Refined
+//
+//  Created by Vincent on 23/06/2017.
+//  Copyright © 2017 zssr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
